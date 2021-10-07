a5f57c3c12b9a61f14cec13178d49e5c2bc8273f
YunYoonBin <yubiny287@gmail.com>
2021-10-07 21:39:56 +0900

    웹페이지

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
 
+function Food( {name, image}) {
+  return <div>
+    <h1>I Like {name}</h1>
+    <img src={image} alt="" />
+  </div>
+}
+
+const foodILike = [
+  {
+  name: "Kimchi",
+  image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
+  },
+  {
+  name: "samgyetang",
+  image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
+  }
+]
+
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <div>
+    {foodILike.map(dish => ( <Food name = {dish.name} image = {dish.image} /> ))}
+  </div>
   );
 }
 
